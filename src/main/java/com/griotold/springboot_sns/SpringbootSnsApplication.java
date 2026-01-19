35e8dba4e522b96bdbe0a05c419fdc49aff40ea2
griotold <gotjd9773@naver.com>
2026-01-19 10:02:32 +0900

    Initial commit

---
+++ src/main/java/com/griotold/springboot_sns/SpringbootSnsApplication.java
@@ -0,0 +1,13 @@
+package com.griotold.springboot_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSnsApplication.class, args);
+	}
+
+}
